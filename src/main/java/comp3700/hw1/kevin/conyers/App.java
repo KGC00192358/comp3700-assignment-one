52bf08cc8ce739a13c74a1948f74e689038bf998
kgc0019 <kgc0019@tigermail.auburn.edu>
2019-08-23 09:20:53 -0500

    initial commit

---
+++ src/main/java/comp3700/hw1/kevin/conyers/App.java
@@ -0,0 +1,13 @@
+package comp3700.hw1.kevin.conyers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
